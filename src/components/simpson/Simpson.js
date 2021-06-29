ce18f0569c97f5d8e9b49bafcd7e17530e699062
viktory11dp <viktory11dp@gmail.com>
2021-06-29 13:51:52 +0300

    lesson1s-components-props-let{item}-сокращаем

--- src/components/simpson/Simpson.js
+++ src/components/simpson/Simpson.js
@@ -1,11 +1,18 @@
 export default  function Simpson(props) {
-    let {item} = props;
+
+    // let {item} = props;
+    // let {name, surname, age, photo, info} = item;
+    //две закоменченные строчки заменяем на
+    // let {item:{name, surname, age, photo, info}} = props;
+    //тем самым еще сокращая код
+
+    let {item:{name, surname, age, photo, info}} = props;
 
     return(
         <div>
-            <h2>{item.name}  {item.surname}  age - {item.age} </h2>
-            <img src={item.photo} alt=""/>
-            <div>{item.info}</div>
+            <h2>{name}  {surname}  age - {age} </h2>
+            <img src={photo} alt=""/>
+            <div>{info}</div>
         </div>
     );
 }
